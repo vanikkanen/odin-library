14cfc14c7ad5cc0aad433a8e0fd70f961c3bb4b8
Valtteri Nikkanen <valtteri.nikkanen@valmet.com>
2025-02-20 07:45:13 +0200

    Add a button to the book cards to toggle their read status

--- script.js
+++ script.js
@@ -13,6 +13,10 @@ function Book(title, author, pages, year, read) {
     this.read = read
 }
 
+Book.prototype.toggleRead = function () {
+    this.read = !this.read
+}
+
 function addBookToLibrary(title, author, pages, year, read) {
     const newBook = new Book(title, author, pages, year, read)
     myLibrary.push(newBook)
@@ -58,6 +62,11 @@ function booksToPage() {
                     <svg fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
                     <path d="M6,19A2,2 0 0,0 8,21H16A2,2 0 0,0 18,19V7H6V19M8,9H16V19H8V9M15.5,4L14.5,3H9.5L8.5,4H5V6H19V4H15.5Z" />
                     </svg>
+                </button> 
+                <button class="read-button" data-index="${idx}">
+                    <svg fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                    <path d="M21.59,11.59L23,13L13.5,22.5L8.42,17.41L9.83,16L13.5,19.68L21.59,11.59M4,16V3H6L9,3A4,4 0 0,1 13,7C13,8.54 12.13,9.88 10.85,10.55L14,16H12L9.11,11H6V16H4M6,9H9A2,2 0 0,0 11,7A2,2 0 0,0 9,5H6V9Z" />
+                    </svg>
                 </button>
             </div>
         `
@@ -68,6 +77,10 @@ function booksToPage() {
             booksToPage()
         })
 
+        const readButton = card.querySelector(".read-button")
+        readButton.addEventListener("click", () => {
+            book.toggleRead()
+        })
         pageLibrary.appendChild(card)
     });
     pageLibrary.appendChild(addCard)
